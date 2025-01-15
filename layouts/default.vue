4912ee7336dda5379843c4cbdc05a7a2465bd7a2
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-15 06:39:45 -0300

    Creating app layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mt-5 color"> 
+        <div class="my-5 text-center">
+            <h1 class="">ToDo App</h1>
+            <span class="">A simple ToDo App by Jeff Brito</span>
+        </div>
+        <div class="">
+            <slot></slot>
+        </div>
+        <div class="text-center mt-5 mb-5">
+            <p>© 2025 Jeff Brito</p>
+        </div>
+    </div>
+</template>
