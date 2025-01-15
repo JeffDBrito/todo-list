11cd05896b4e5f06b94914ab1f963c3a6e0d7e23
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-15 13:49:30 -0300

    implementing toast notification and date filter

--- components/task/TaskList.vue
+++ components/task/TaskList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row mb-5">
         <div class="col-md-9">
-            <DateFilter @filterByDate="filterByDate"/>
+            <DateFilter @filterByDate="reloadTasks"/>
         </div>
         <div class="col-md-3">
             <TaskNewTask :taskCount="taskCount" @newTask="newTask"/>
@@ -10,23 +10,29 @@
         <div class="col-md-12 mt-5">
             <div class="mt-5">
                 <div class="mt-5 row">
-                    <h3 class="col-11">Uncompleted Tasks</h3>
-                    <input class="col-1 btn text-white text-sm" type="button" @click="toggleUncompleted()" :value="show_uncompleted ? 'v' : '>'"/>
+                    <h3 class="col-10">Pending Tasks</h3>
+                    <div class="col-1 row align-content-center justify-content-center">
+                        <span class="rounded bg-dark-purple">{{ pending_tasks.length }}</span>
+                    </div>
+                    <input class="col-1 btn text-white text-sm" type="button" @click="togglePending()" :value="show_pending ? 'v' : '>'"/>
                 </div>
                 <hr>
-                <div class="" v-if="show_uncompleted">
-                    <Task v-for="(task, index) in uncompleted_tasks" :key="task.id" @deleteTask="deleteTask" @toggleStatus="updateTasks" :task="task" :index="index"/>
+                <div class="" v-if="show_pending">
+                    <Task v-for="(task, index) in pending_tasks" :key="task.id" @updateTask="updateTask" @deleteTask="deleteTask" @toggleStatus="toggleStatus" :task="task" :index="index"/>
                 </div>
                     
             </div>
             <div class="mt-5">
                 <div class="mt-5 row">
-                    <h3 class="col-11">Completed Tasks</h3>
-                    <input class="col-1 btn text-white text-sm" type="button" @click="toggleCompleted()" :value="show_completed ? 'v' : '>'"/>
+                    <h3 class="col-10">Done Tasks</h3>
+                    <div class="col-1 row align-content-center justify-content-center">
+                        <span class="rounded bg-dark-purple">{{ done_tasks.length }}</span>
+                    </div>
+                    <input class="col-1 btn text-white text-sm" type="button" @click="toggleDone()" :value="show_done ? 'v' : '>'"/>
                 </div>
                 <hr>
-                <div class="" v-if="show_completed">
-                    <Task v-for="(task, index) in completed_tasks" :key="task.id" @deleteTask="deleteTask" @toggleStatus="updateTasks" :task="task" :index="index"/>
+                <div class="" v-if="show_done">
+                    <Task v-for="(task, index) in done_tasks" :key="task.id" @deleteTask="deleteTask" @toggleStatus="toggleStatus" :task="task" :index="index"/>
                 </div>
             </div>
         </div>
@@ -39,26 +45,33 @@ import { ref, reactive } from 'vue'
 
 const toast = useToast()
 
-const today = new Date()
+const today = new Date().toJSON().slice(0,10).replace(/-/g,'-');
 let all_tasks = ref([])
 let taskCount = ref(0)
-let uncompleted_tasks = ref([])
-let completed_tasks = ref([])
-let show_uncompleted = ref(true)
-let show_completed = ref(true)
+let pending_tasks = ref([])
+let done_tasks = ref([])
+let show_pending = ref(true)
+let show_done = ref(true)
 
 let selected_date = ref('')
 
-const filterByDate = (date = null) => { 
-    console.log(date)
+const togglePending = (show) => {
+    show_pending.value = !show_pending.value
 }
 
-const toggleUncompleted = (show) => {
-    show_uncompleted.value = !show_uncompleted.value
+const toggleDone = (show) => {
+    show_done.value = !show_done.value
 }
 
-const toggleCompleted = (show) => {
-    show_completed.value = !show_completed.value
+const toggleStatus = (task) => {
+
+    let title = (task.status ? 'Pending!' : 'Done!')
+    let description = (task.status ? 'Task marked as pending.' : 'Task marked as done.')
+    let color = (task.status ? 'warning' : 'success')
+    
+    showToast(title, description, color)
+
+    reloadTasks()
 }
 
 const newTask = (task) => {
@@ -70,25 +83,63 @@ const newTask = (task) => {
         description: task.description
     }
 
-    toast.add({
-        title: 'Done!',
-        description: 'Task created with success.',
-        color: 'success'
-    })
+    showToast('Done!','Task created with success.','success')
 
     all_tasks.value.push(new_task)
     taskCount.value = taskCount.value + 1
-    updateTasks()
+    reloadTasks()
 }
 
-const updateTasks = () => {
-    uncompleted_tasks.value = all_tasks.value.filter(task => task.status === false)
-    completed_tasks.value = all_tasks.value.filter(task => task.status === true)
+const reloadTasks = (date = null) => {
+
+    if(!date || date == '' || date == 'all'){
+        pending_tasks.value = all_tasks.value.filter(task => task.status === false)
+        done_tasks.value = all_tasks.value.filter(task => task.status === true)
+    }else if(date === 'today') {
+        pending_tasks.value = all_tasks.value.filter(task => task.status === false && task.date === today)
+        done_tasks.value = all_tasks.value.filter(task => task.status === true && task.date === today)
+    }else{
+        pending_tasks.value = all_tasks.value.filter(task => task.status === false && task.date === date)
+        done_tasks.value = all_tasks.value.filter(task => task.status === true && task.date === date)
+    }
+
+
 }
 
 const deleteTask = (task) => {
     all_tasks.value = all_tasks.value.filter(t => t !== task)
-    updateTasks()
+
+    showToast('Done!','Task deleted!','error')
+    
+    reloadTasks()
+}
+
+const updateTask = (details) => {
+
+    let task = details[0]
+    let updated_task = details[1]
+
+    task.title = updated_task.title
+    task.status = updated_task.status
+    task.date = updated_task.date
+    task.description = updated_task.description
+
+    let title = 'Done!'
+    let description = 'Task updated with success.'
+    let color = 'success'
+
+    showToast(title, description, color)
+
+    reloadTasks()
+}
+
+const showToast = (title, description, color) => {
+    toast.clear()
+    toast.add({
+        title: title,
+        description: description,
+        color: color
+    })
 }
 
 
