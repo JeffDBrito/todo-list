871f2e3a9a9ab65213fb0b1023c7f76a1ad51a7f
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-14 23:14:29 -0300

    Creating index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row col-md-6">
+        <div class="col-md-9">
+            <DateFilter />
+        </div>
+        <div class="col-md-3">
+            <TaskNewTask />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
