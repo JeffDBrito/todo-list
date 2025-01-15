481d4b38495afec022d5e10fbd91d897f352c49d
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-15 06:42:03 -0300

    adding functions to the DateFilter component

--- components/DateFilter.vue
+++ components/DateFilter.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="row">
-        <div class="col-md-2">
-            <input class="form-control btn btn-primary" type="button" value="all" @click="filterByDate('all')"/>
-        </div>
-        <div class="col-md-2">
-            <input class="form-control btn btn-primary" type="button" value="today" @click="filterByDate('today')" />
+        <div class="row col-md-4">
+            <input class="mx-2 col-md-5 btn bg-dark-purple" type="button" value="All" @click="$emit('filterByDate','all')"/>
+            <input class="mx-2 col-md-5 btn bg-dark-purple" type="button" value="Today" @click="$emit('filterByDate','today')" />
         </div>
         <div class="col-md-8">
-            <input class="form-control" type="date" v-model="selected_date" @change="filterByDate('date')" />
+            <input class="form-control" type="date" v-model="selected_date" @change="$emit('filterByDate',getSelectedDate())" />
         </div>
     </div>
 </template>
@@ -16,15 +14,13 @@
 
 import { ref } from 'vue'
 
+defineEmits(['filterByDate'])
+
 const selected_date = ref('')
 
-const filterByDate = (date = null) => { 
+const getSelectedDate = () => { 
 
-    if(date === 'date'){
-        console.log(selected_date.value)
-    }else{
-        console.log(date)
-    }
+    return selected_date.value
 
 }
 
