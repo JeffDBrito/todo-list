5ed9286199fbe25653ee10c1011a10b4667ce931
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-15 13:48:11 -0300

    adding form validation

--- components/task/NewTask.vue
+++ components/task/NewTask.vue
@@ -15,24 +15,26 @@
                     <div class="modal-body">
                         <div class="row">
                             <div class="mt-3 col-6">
-                                <label class="ml-5" for="title">Title</label>
+                                <label class="ml-2 mb-2 text-black" for="title">Title</label>
                                 <input v-model="newTask.title" id="title" class="form-control" type="text" placeholder="My new task" />
+                                <span class="text-sm ml-3 mt-2 text-danger"> {{ errors.title }} </span>
                             </div>
                             <div class="mt-3 col-6">
-                                <label class="ml-5" for="date">Date</label>
+                                <label class="ml-2 mb-2 text-black" for="date">Date</label>
                                 <input v-model="selected_date" id="date" class="form-control" type="date" @change="setDate()"/>
+                                <span class="text-sm ml-3 mt-2 text-danger"> {{ errors.date }} </span>
                             </div>
                         </div>
                         <div class="mt-3 col-12">
-                            <label class="ml-5" for="description">Description</label>
+                            <label class="ml-2 mb-2 text-black" for="description">Description</label>
                             <textarea v-model="newTask.description" id="description" class="form-control" type="" placeholder="This is my new task"></textarea>
                         </div>
                     </div>
 
                     <div class="mt-3 col-12 row justify-content-end modal-footer">
                         <div class="row col-10">
-                            <input class="col-1 p-2 form-check-input" type="checkbox" name="completed" id="completed" v-model="newTask.status">
-                            <label class="col-7 text-black" for="completed">Mark as completed</label>
+                            <input class="col-1 p-2 form-check-input" type="checkbox" name="done" id="done" v-model="newTask.status">
+                            <label class="col-7 text-black" for="done">Mark as done</label>
                         </div>
 
                         <input id="save" class="col-2 btn bg-light-purple align-self-end" type="button" @click="processNewTask()" value="Save">
@@ -61,16 +63,42 @@ let newTask = reactive({
     date: '',
     description: ''
 })
+let errors = reactive({
+    title: '',
+    date: ''
+})
 
 const showAddModal = () => {
+    errors.title = ''
+    errors.date = ''
+    selected_date.value = ''
     showModal.value = true
 }
 
-function setDate(){
+const setDate = () => {
     newTask.date = selected_date.value
 }
 
-function processNewTask(){
+const resetErrors = () => {
+    errors.title = ''
+    errors.date = ''
+}
+
+const processNewTask = () => {
+
+    resetErrors()
+
+    if(newTask.title == ''){
+        errors.title = 'Title is required'
+    }
+
+    if(newTask.date == ''){
+        errors.date = 'Date is required'
+    }
+
+    if(errors.title != '' || errors.date != ''){
+        return
+    }
     
     emit('newTask',newTask)
 
