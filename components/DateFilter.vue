627a9481f14b329a6372037f1b93c29f96e4de16
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-01-14 23:13:12 -0300

    creating and testing datefilter

---
+++ components/DateFilter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-md-2">
+            <input class="form-control btn btn-primary" type="button" value="all" @click="filterByDate('all')"/>
+        </div>
+        <div class="col-md-2">
+            <input class="form-control btn btn-primary" type="button" value="today" @click="filterByDate('today')" />
+        </div>
+        <div class="col-md-8">
+            <input class="form-control" type="date" v-model="selected_date" @change="filterByDate('date')" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+const selected_date = ref('')
+
+const filterByDate = (date = null) => { 
+
+    if(date === 'date'){
+        console.log(selected_date.value)
+    }else{
+        console.log(date)
+    }
+
+}
+
+</script>
